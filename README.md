785bc73ac43121064389349522bf6c948d90ba60
Dvora C <68243452+dicelniker@users.noreply.github.com>
2022-09-22 17:15:40 -0700

    Test Commit with Eitan

---
+++ README.md
@@ -0,0 +1,2 @@
+# Team-Atlas_2022Fall-ARC-Code
+This repo is for Volare
---
+++ main.py
@@ -0,0 +1,18 @@
+#Import Modules
+from djitellopy import Tello
+import cv2
+drone = Tello()
+
+#Connect before anything
+drone.connect()
+#Start Code
+drone.takeoff()
+drone.streamon()
+
+drone.flip_forward()
+
+drone.streamoff()
+drone.land()
+#End Code
+
+#Eitan's Test Commit
