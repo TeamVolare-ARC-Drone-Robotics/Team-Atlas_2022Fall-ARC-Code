622558843eef322949feda1c47ff5d1aab293e75
shafanoam <109098752+shafanoam@users.noreply.github.com>
2022-09-29 16:09:46 -0700

    test git commit

--- Main-Tello-Code/main.py
+++ Main-Tello-Code/main.py
@@ -1,18 +1,18 @@
-#Import Modules
+# Import Module
 from djitellopy import Tello
 import cv2
 import GoTo_Ballons
 
 drone = Tello()
 
-#Connect before anything
+# Connect before anything
 drone.connect()
-#Start Code
+# Start Code
 drone.takeoff()
 drone.streamon()
 
-#Go To the Ballons
+# Go To the Ballons
 
+# End Code
 drone.streamoff()
 drone.land()
-#End Code
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Team-Atlas_2022Fall-ARC-Code
-This repo is for Volare
+This repo is for Team Volare of the Aerospace Robotics Competition, 2022
