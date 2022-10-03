764ed8ccc30a0877af89e967598d90e523851b74
k-Jandhyala <60835325+k-Jandhyala@users.noreply.github.com>
2022-10-03 10:45:37 -0700

    Update main.py

--- Main-Tello-Code/main.py
+++ Main-Tello-Code/main.py
@@ -72,11 +72,8 @@ drone.streamon()
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image containing ArUCo tag")
-ap.add_argument("-t", "--type", type=str,
-	default="DICT_ARUCO_ORIGINAL",
-	help="type of ArUCo tag to detect")
+ap.add_argument("-i", "--image", required=True, help="path to input image containing ArUCo tag")
+ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="type of ArUCo tag to detect")
 args = vars(ap.parse_args())
 
 # define names of each possible ArUco tag OpenCV supports
