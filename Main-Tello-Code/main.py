537333e1e9d16974251af6b18afe41973d08c2c9
shafanoam <109098752+shafanoam@users.noreply.github.com>
2022-10-03 19:05:00 -0700

    corrected ballons->balloons and added initilization of arrays

--- Main-Tello-Code/main.py
+++ Main-Tello-Code/main.py
@@ -10,12 +10,35 @@ import sys
 
 drone = Tello()
 
+# Pre-initalize arrays, both faster to execute and easier to visualise
+
+# Tag array
+tag_list = [
+			[0,0,0,0,0,0]
+			[0,0,0,0,0,0]
+			[0,0,0,0,0,0]
+			]
+
+# Pop array
+pop_list = [
+			[0,0,0,0,0,0]
+			[0,0,0,0,0,0]
+			[0,0,0,0,0,0]
+			]
+
+# Movement array
+mov_list = [
+			[0,0,0,0,0,0]
+			[0,0,0,0,0,0]
+			[0,0,0,0,0,0]
+			]
+
 #Start Writing Functions
-def popBallons():
+def popBalloon():
     drone.move_forward(20)
     drone.move_back(20)
 
-def traverseBallons(popArray):
+def traverseBalloons(popArray):
     #detect Marker and if maker is the needed maker then call pop
     pass
 
@@ -25,7 +48,7 @@ def traverseBallons(popArray):
     	drone.move_right(20)
     	for j in range(len(popArray)):
     		if(popArray[j] == #detected Tag number):
-    			popBallons()
+    			popBalloons()
     			i += 1
     		else:
     			i+= i
@@ -40,21 +63,23 @@ def traverseBallons(popArray):
 
 
 # Connect before anything
-drone.connect()
+def initalize():
+	drone.connect()
+	drone.streamon()
+
 # Start Code
 drone.takeoff()
-drone.streamon()
 
 # Start Code
 popArray = [#Manually Feed Numbers#]
-traverseBallons(popArray)
+traverseBalloons(popArray)
 
 # End Code
 drone.streamoff()
 drone.land()
 
 """
-#Run the test to see ArUCo Tag numbers
+# Run the test to see ArUCo Tag numbers
 # import the necessary packages
 import argparse
 import imutils
@@ -69,7 +94,6 @@ drone.connect()
 drone.takeoff()
 drone.streamon()
 
-
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image containing ArUCo tag")
@@ -78,27 +102,27 @@ args = vars(ap.parse_args())
 
 # define names of each possible ArUco tag OpenCV supports
 ARUCO_DICT = {
-	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-#	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-#	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-#	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-#	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    #	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    #	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    #	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    #	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
 
 # load the input image from disk and resize it
@@ -109,9 +133,9 @@ image = imutils.resize(image, width=600)
 # verify that the supplied ArUCo tag exists and is supported by
 # OpenCV
 if ARUCO_DICT.get(args["type"], None) is None:
-	print("[INFO] ArUCo tag of '{}' is not supported".format(
-		args["type"]))
-	sys.exit(0)
+    print("[INFO] ArUCo tag of '{}' is not supported".format(
+        args["type"]))
+    sys.exit(0)
 
 # load the ArUCo dictionary, grab the ArUCo parameters, and detect
 # the markers
@@ -119,44 +143,44 @@ print("[INFO] detecting '{}' tags...".format(args["type"]))
 arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
 arucoParams = cv2.aruco.DetectorParameters_create()
 (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
-	parameters=arucoParams)
+                                                   parameters=arucoParams)
 
 # verify *at least* one ArUco marker was detected
 if len(corners) > 0:
-	# flatten the ArUco IDs list
-	ids = ids.flatten()
+    # flatten the ArUco IDs list
+    ids = ids.flatten()
 
-	# loop over the detected ArUCo corners
-	for (markerCorner, markerID) in zip(corners, ids):
-		# extract the marker corners (which are always returned in
-		# top-left, top-right, bottom-right, and bottom-left order)
-		corners = markerCorner.reshape((4, 2))
-		(topLeft, topRight, bottomRight, bottomLeft) = corners
+    # loop over the detected ArUCo corners
+    for (markerCorner, markerID) in zip(corners, ids):
+        # extract the marker corners (which are always returned in
+        # top-left, top-right, bottom-right, and bottom-left order)
+        corners = markerCorner.reshape((4, 2))
+        (topLeft, topRight, bottomRight, bottomLeft) = corners
 
-		# convert each of the (x, y)-coordinate pairs to integers
-		topRight = (int(topRight[0]), int(topRight[1]))
-		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-		topLeft = (int(topLeft[0]), int(topLeft[1]))
+        # convert each of the (x, y)-coordinate pairs to integers
+        topRight = (int(topRight[0]), int(topRight[1]))
+        bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+        bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+        topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-		# draw the bounding box of the ArUCo detection
-		cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-		cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-		cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-		cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+        # draw the bounding box of the ArUCo detection
+        cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+        cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+        cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+        cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
 
-		# compute and draw the center (x, y)-coordinates of the ArUco
-		# marker
-		cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-		cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-		cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+        # compute and draw the center (x, y)-coordinates of the ArUco
+        # marker
+        cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+        cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+        cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
 
-		# draw the ArUco marker ID on the image
-		cv2.putText(image, str(markerID),
-			(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-			0.5, (0, 255, 0), 2)
-		print("[INFO] ArUco marker ID: {}".format(markerID))
+        # draw the ArUco marker ID on the image
+        cv2.putText(image, str(markerID),
+                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
+        print("[INFO] ArUco marker ID: {}".format(markerID))
 
-		# show the output image
-		cv2.imshow("Image", image)
-		cv2.waitKey(0)
+        # show the output image
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
