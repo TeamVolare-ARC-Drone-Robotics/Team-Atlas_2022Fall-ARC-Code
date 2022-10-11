436548c986bd928f340897023e5ac11e0d7946c9
k-Jandhyala <60835325+k-Jandhyala@users.noreply.github.com>
2022-10-10 22:07:12 -0700

    Drone camera works, with face detection test

--- Main-Tello-Code/main.py
+++ Main-Tello-Code/main.py
@@ -1,74 +1,45 @@
-# Import Module
 from djitellopy import Tello
 import cv2
-# import keyboard
+import math
+import time
 
 drone = Tello()
 
-# #Start Writing Functions
-# def popBallons():
-#     drone.move_forward(20)
-
-
-# Connect before anything
 drone.connect()
+drone.takeoff()
+time.sleep(1)
 
+drone.streamon()
 
-# Test Movement
-# drone.flip_forward()
+cv2.namedWindow("drone")
+drone_stream = drone.get_frame_read()
 
-# Go To the Balloons
-def droneInformation():
-    print("Battery: ", drone.get_battery(),"%")
-    print("Yaw: ", drone.get_yaw())
-    print("Roll: ", drone.get_roll())
-    print("Pitch: ", drone.get_pitch())
-    print("Height: ", drone.get_height())
-    print("Pressure: ", drone.get_barometer())
-    print("Flight Time: ", drone.get_flight_time())
+#Import the face cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-land = False
-while land == True:
+while True:
+	#Read the incoming frames
+	stream = drone_stream.frame
+	#convert th frames to a grayscale image
+	grayscale = cv2.cvtColor(stream, cv2.COLOR_BGR2GRAY)
+	#process the image with comparision the face cascade and detect the face
+	faces = face_cascade.detectMultiScale(grayscale, 1.1, 4)
+	#If there is a face then
+	for (x, y, w, h) in faces:
+		cv2.rectangle(stream, (x, y), (x + w, y + h), (255, 0, 0), 2)
+	cv2.imshow('Tello Footage', stream)
 
-    print(droneInformation())
-
-# This part might work for the steam
-    drone.streamon()
-
-    while True:
-        img = me.get_frame_read().frame
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-
-    key = input("Key Board Input: ")
-    if key == "land":  # ESC
-        land = True
-    elif key == 'w':
-        drone.move_forward(30)
-    elif key == 's':
-        drone.move_back(30)
-    elif key == 'a':
-        drone.move_left(30)
-    elif key == 'd':
-        drone.move_right(30)
-    elif key == 'e':
-        drone.rotate_clockwise(30)
-    elif key == 'q':
-        drone.rotate_counter_clockwise(30)
-    elif key == 'r':
-        drone.move_up(30)
-    elif key == 'f':
-        # This section is to attempt to prevent the massive errors and stalls that happen when trying to
-        # make the drone go down when it is close to the ground.
-        try:
-            drone.move_down(30)
-        except:
-            print("An error has been caught- perhaps the drone is too low to the ground?" +
-                  f"\nIf so, make sure to use the 'land' command to get it to touch down.")
-
-
-# End Code
-drone.streamoff()
-drone.land()
+	key = cv2.waitKey(1) & 0xff
+	if key == ord('q'):
+		drone.land()
+		drone_stream.stop()
+		drone.streamoff()
+		exit(0)
+	elif key == ord('w'):
+		drone.move_up(30)
+	elif key == ord('s'):
+		drone.move_down(30)
+	elif key == ord('a'):
+		drone.rotate_counter_clockwise(20)
+	elif key == ord('d'):
+		drone.rotate_clockwise(20)
