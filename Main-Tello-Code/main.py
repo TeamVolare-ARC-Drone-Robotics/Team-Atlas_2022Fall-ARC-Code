c362b89b3faf3dd149cae2a248d6b116363d9885
shafanoam <109098752+shafanoam@users.noreply.github.com>
2022-10-13 15:42:30 -0700

    Inital setup of code arrangement, beginings of tagarray-to-poparray function

--- Main-Tello-Code/main.py
+++ Main-Tello-Code/main.py
@@ -1,1 +1,42 @@
+# Initialize the arrays
+taglist = []
 
+tagarray=[
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0
+]
+
+poparray=[
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0
+]
+
+movearray=[
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0
+]
+
+
+def detect_tags():
+    pass
+
+
+def decide_pops(tags):
+    i = 0
+    for num in len(tagarray):
+        if tagarray(i)
+            poparray[i] = 1
+        else:
+            poparray[i] = 0
+
+
+def movedecider(type):
+    if type == "dumb":
+        pass
+    elif type == "traveler":
+        pass
+    else:
+        print("Major error! Make sure to specify the movement tactic for 'movedecider', either 'dumb' or 'traveler'")
